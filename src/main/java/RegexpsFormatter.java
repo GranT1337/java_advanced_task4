27d407ef12ff723052fd9354d4b35a74df54ef7e
Ostanin Kirill <grant1337@pm.me>
2019-05-06 16:03:19 +0400

    v1.1 with replace +5 on 6 and etc.

--- src/main/java/RegexpsFormatter.java
+++ src/main/java/RegexpsFormatter.java
@@ -6,12 +6,13 @@ public class RegexpsFormatter {
 
     public static String formatMobileNumber(String string) {
         String defaultStr = string;
-        Pattern pattern = Pattern.compile("\\+\\d{1}\\({1}\\d{3}\\){1}\\s\\d{3}\\s\\d{2}\\s\\d{2}");
+        Pattern pattern = Pattern.compile("\\+\\d\\(\\d{3}\\)\\s\\d{3}\\s\\d{2}\\s\\d{2}");
         Matcher matcher = pattern.matcher(string);
 
         while (matcher.find()) {
             String mobileNumber = matcher.group();
-            String correctMobileNumber = mobileNumber.replace("+7", "+8")
+            String newY = String.valueOf(Integer.valueOf(mobileNumber.substring(1,2)) + 1);
+            String correctMobileNumber = mobileNumber.replaceAll("\\+\\d", newY)
                                                    .replaceAll("\\W", "");
             defaultStr = defaultStr.replace(mobileNumber, correctMobileNumber);
         }
